628bb21689ad26d3e0b301940e8ac918b32aaf7f
productioncoder <jan@productioncoder.com>
2020-05-20 22:27:40 +0200

    rename test description

--- test/integration/dev.spec.js
+++ test/integration/dev.spec.js
@@ -2,9 +2,9 @@ const request = require('supertest');
 const assert = require('assert');
 const Server = require('../../server');
 
-describe('create developer', () => {
+describe('developer', () => {
   const { app } = new Server();
-  it('should create a developer', async () => {
+  it('creates a developer', async () => {
     const res = await request(app).post('/dev').send({
       email: 'jan@productioncoder.com',
       firstName: 'Jan',
